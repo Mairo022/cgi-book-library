0ce7a1284212ce143277f2e7ebcf95fb4b26fe9a
Mairo022 <118812987+Mairo022@users.noreply.github.com>
2023-03-17 19:33:43 +0200

    Add roles and logging actions for navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,14 +1,52 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import "../styles/components/navbar.scss"
+import { useUserDetails } from "../context/userContext";
 
 function Navbar(): JSX.Element {
-    return (
-        <nav className="nav">
-            <NavLink className="nav__item" to="/books">Books</NavLink>
-            <NavLink className="nav__item" to="/checkouts">Checkouts</NavLink>
-            <button type="button">Login</button>
-        </nav>
-    )
+    const {role, setRole, setIsLoggedIn} = useUserDetails()
+    const navigate = useNavigate()
+
+    function logOut(): void {
+        setRole(undefined)
+        setIsLoggedIn(false)
+        localStorage.removeItem("isLoggedIn")
+        localStorage.removeItem("role")
+    }
+
+    function logIn(): void {
+        navigate("/login")
+    }
+
+    function navbar(): JSX.Element {
+        if (role === "reader") {
+            return (
+                <nav className="nav">
+                    <NavLink className="nav__item" to="/books">Books</NavLink>
+                    <NavLink className="nav__item" to="/books/favourites">My Books</NavLink>
+                    <NavLink className="nav__item" to="/checkouts/my-checkouts">My Checkouts</NavLink>
+                    <button className="nav__item nav__btn" onClick={() => {logOut()}}>Logout</button>
+                </nav>
+            )
+        }
+        if (role === "librarian") {
+            return (
+                <nav className="nav">
+                    <NavLink className="nav__item" to="/books">Books</NavLink>
+                    <NavLink className="nav__item" to="/checkouts">Checkouts</NavLink>
+                    <button className="nav__item nav__btn" onClick={() => {logOut()}}>Logout</button>
+                </nav>
+            )
+        }
+
+        return (
+            <nav className="nav">
+                <NavLink className="nav__item" to="/books">Books</NavLink>
+                <button className="nav__item nav__btn" onClick={() => {logIn()}}>Login</button>
+            </nav>
+        )
+    }
+
+    return navbar()
 }
 
 export default Navbar
